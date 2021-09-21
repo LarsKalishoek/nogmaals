20a3184945a8b8071c99dea530d9133befa0a0f5
arsKalishoek <99069312@mydavinci.nl>
2021-09-21 10:14:42 +0200

    oefening 10

--- robotarm-python-2021-main/robotarm-python-2021-main/example10.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/example10.py
@@ -11,8 +11,7 @@ for x in range(9, 0, -2):
     for d in range(0, x):
         robotArm.moveRight()
     robotArm.drop()
-    x = x - 1
-    for y in range(0, x):
+    for y in range(0, x - 1):
         robotArm.moveLeft()
     
 
