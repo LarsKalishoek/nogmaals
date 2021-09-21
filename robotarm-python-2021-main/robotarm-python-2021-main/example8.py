b1f665b72a933c36af352afba2ade65407ee64b6
arsKalishoek <99069312@mydavinci.nl>
2021-09-21 09:36:28 +0200

    oefening8

---
+++ robotarm-python-2021-main/robotarm-python-2021-main/example8.py
@@ -0,0 +1,17 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 8')
+
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.moveRight()
+for z in range(0, 7):
+    robotArm.grab()
+    for x in range(0, 9):
+        robotArm.moveRight()
+    robotArm.drop()
+    for y in range (1, 9):
+        robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
